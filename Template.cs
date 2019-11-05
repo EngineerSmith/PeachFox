4626d59f21db514a304eeaddbfe46bef07e80be4
EngineerSmith <pasmith.bus@gmail.com>
2019-11-05 13:11:49 +0400

    Added templating scheme which just find and replaces certain keywords

---
+++ Template.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeachFox
+{
+    static class Template
+    {
+        public static string FillTemplate(string template, Dictionary<string, string> dictionary)
+        {
+            const string whitespace = " ";
+            var arr = template.Split(whitespace.ToCharArray());
+            for (int i = 0; i < arr.Length; i++)
+            {
+                switch (arr[i])
+                {
+                    case "TABLE":
+                        {
+                            InsertTable(ref arr, ref i, dictionary);
+                            break;
+                        }
+                    default:
+                        continue;
+
+                }
+            }
+            return string.Join(whitespace, arr); ;
+        }
+
+        private static void InsertTable(ref string[] arr, ref int index, Dictionary<string, string> dictionary)
+        {
+            if (IsValidate(arr, index + 1) == false)
+            {
+                arr[index] = "";
+                return;
+            }
+            string key = arr[index + 1];
+            arr[index + 1] = "\n";
+            string value = dictionary[key];
+            if (value == null)
+            {
+                arr[index] = "";
+                return;
+            }
+            arr[index] = value;
+            index++;
+        }
+
+        private static bool IsValidate(string[] arr, int index)
+        {
+            return arr.ElementAtOrDefault(index) != null;
+        }
+    }
+}
